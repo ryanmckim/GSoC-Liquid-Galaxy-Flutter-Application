e38591db7266e38df1f35f5ace71a190db8dcdeb
ryanmckim <mck649@gmail.com>
2023-02-16 11:51:28 -0800

    Finished Video page

--- demo_flutter_application/lib/video.dart
+++ demo_flutter_application/lib/video.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class Video extends StatefulWidget {
   const Video({super.key});
@@ -8,9 +9,36 @@ class Video extends StatefulWidget {
 }
 
 class _StateVideo extends State<Video> {
+  late YoutubePlayerController ytController;
+
+  @override
+  void initState() {
+    const videoID = "ACN1oWT79gQ";
+    ytController = YoutubePlayerController(
+        initialVideoId: videoID,
+        flags: const YoutubePlayerFlags(
+            autoPlay: false, mute: false, isLive: false));
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        backgroundColor: Colors.white, body: Center(child: Text("video")));
+    return Scaffold(
+        appBar: AppBar(title: const Text("GSoC Liquid Galaxy Videos")),
+        body: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          const SizedBox(height: 20),
+          const Text(
+            "Introduction :)",
+            style: TextStyle(fontSize: 30),
+          ),
+          YoutubePlayer(
+            controller: ytController,
+            showVideoProgressIndicator: true,
+          ),
+          const SizedBox(height: 40),
+          const Text("Flutter & VS Code Setup Video (Coming soon!)",
+              style: TextStyle(fontSize: 30), textAlign: TextAlign.center)
+        ]));
   }
 }
