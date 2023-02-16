2f41901c8eb8ac8621add3848021bf2b91326f84
ryanmckim <mck649@gmail.com>
2023-02-15 22:52:39 -0800

    Finished home page

---
+++ demo_flutter_application/lib/lg.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LiquidGalaxy extends StatefulWidget {
+  const LiquidGalaxy({super.key});
+
+  @override
+  State<LiquidGalaxy> createState() => _LiquidGalaxyState();
+}
+
+class _LiquidGalaxyState extends State<LiquidGalaxy> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(child: Text("bye")),
+    );
+  }
+}
